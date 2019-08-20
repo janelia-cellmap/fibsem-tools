3b3a1a276c4e86710d11baa8fc362a2e442eeca0
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-08-20 17:24:04 -0400

    changed default fill mode to minimum in padstack

--- fst/ingest.py
+++ fst/ingest.py
@@ -3,27 +3,31 @@ import dask.array as da
 from typing import Iterable, Union
 
 
-def padstack(arrays: Iterable[da.Array], fill_value: Union[int, float] = 0) -> da.Array:
+def padstack(arrays: Iterable[da.Array], mode: str = 'minimum', constant_values: Union[int, float] = 0) -> da.Array:
     """
-    Stack arrays with variable axis sizes. A bounding box is calculated across all the arrays
+    Stack arrays with variable axis sizes. A bounding box is calculated across all the arrays and each sub-array is
+    padded to fit within the bounding box. This is a light wrapper around dask.array.pad.
 
     Parameters
     ----------
     arrays : An iterable collection of dask arrays.
-    fill_value : The value to fill when padding images, if necessary.
+    mode: The fill mode to use. See `dask.array.pad`
+    constant_values : The value to fill when padding images, if necessary.
 
     Returns a dask array containing the entire dataset represented by the individual arrays in `arrays`.
     -------
 
     """
+
     shapes = np.array([a.shape for a in arrays])
     bounds = shapes.max(0)
     pad_extent = [
         list(zip((0,) * shapes.shape[1], bounds - np.array(a.shape))) for a in arrays
     ]
-    padded = [
-        da.pad(a, pad_extent[ind], mode="constant", constant_values=fill_value)
-        for ind, a in enumerate(arrays)
-    ]
+    if mode == 'constant':
+        padded = [(da.pad(a, pad_width=pad_extent[ind], mode=mode, constant_values=constant_values)) for ind, a in enumerate(arrays)]
+    else:
+        padded = [(da.pad(a, pad_width=pad_extent[ind], mode=mode)) for ind, a in enumerate(arrays)]
+
     stacked = da.stack(padded)
     return stacked
