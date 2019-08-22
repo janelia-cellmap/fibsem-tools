e76b70a3ab4a4329732c3f8919d00116f385d312
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-08-22 11:39:48 -0400

    padstack now has the option to pad with miniumum-1

--- fst/ingest.py
+++ fst/ingest.py
@@ -1,9 +1,10 @@
 import numpy as np
 import dask.array as da
 from typing import Iterable, Union
+from numbers import Number
+from dask import delayed
 
-
-def padstack(arrays: Iterable[da.Array], mode: str = 'minimum', constant_values: Union[int, float] = 0) -> da.Array:
+def padstack(arrays: Iterable[da.Array], constant_values: Union[int, float, str] = 0) -> da.Array:
     """
     Stack arrays with variable axis sizes. A bounding box is calculated across all the arrays and each sub-array is
     padded to fit within the bounding box. This is a light wrapper around dask.array.pad.
@@ -11,8 +12,7 @@ def padstack(arrays: Iterable[da.Array], mode: str = 'minimum', constant_values:
     Parameters
     ----------
     arrays : An iterable collection of dask arrays.
-    mode: The fill mode to use. See `dask.array.pad`
-    constant_values : The value to fill when padding images, if necessary.
+        constant_values : The value to fill when padding images
 
     Returns a dask array containing the entire dataset represented by the individual arrays in `arrays`.
     -------
@@ -24,10 +24,15 @@ def padstack(arrays: Iterable[da.Array], mode: str = 'minimum', constant_values:
     pad_extent = [
         list(zip((0,) * shapes.shape[1], bounds - np.array(a.shape))) for a in arrays
     ]
-    if mode == 'constant':
-        padded = [(da.pad(a, pad_width=pad_extent[ind], mode=mode, constant_values=constant_values)) for ind, a in enumerate(arrays)]
-    else:
-        padded = [(da.pad(a, pad_width=pad_extent[ind], mode=mode)) for ind, a in enumerate(arrays)]
+
+    if isinstance(constant_values, Number):
+        padded = [(da.pad(a, pad_width=pad_extent[ind], mode='constant', constant_values=constant_values)) for ind, a in enumerate(arrays)]
+    elif constant_values == 'minimum-minus-one':
+        delmin = delayed(np.min)
+        # this will be bad if the minimum value in the data hits the floor of the datatype
+        fill_value = int(min(delayed(delmin(a) for a in arrays).compute()) - 1)
+
+        padded = [(da.pad(a, pad_width=pad_extent[ind], mode='constant', constant_values=fill_value)) for ind, a in enumerate(arrays)]
 
     stacked = da.stack(padded)
     return stacked
