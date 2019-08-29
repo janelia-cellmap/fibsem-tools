2435935deec5f0a64432b155747466d88c70ffb5
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-08-29 13:27:54 -0400

    added dask_jobqueue requirement

--- setup.py
+++ setup.py
@@ -8,44 +8,41 @@ from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
-NAME = 'fst'
-DESCRIPTION = 'Tools for processing FIB-SEM data.'
-URL = 'https://github.com/janelia-cosem/fst'
-EMAIL = 'bennettd@janelia.hhmi.org'
-AUTHOR = 'Davis Bennett'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.0'
-REQUIRED = [
-    'dask', 'numpy', 'zarr'
-]
+NAME = "fst"
+DESCRIPTION = "Tools for processing FIB-SEM data."
+URL = "https://github.com/janelia-cosem/fst"
+EMAIL = "bennettd@janelia.hhmi.org"
+AUTHOR = "Davis Bennett"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.1.0"
+REQUIRED = ["dask", "numpy", "zarr", "dask_jobqueue"]
 
-EXTRAS = {
-   }
+EXTRAS = {}
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
 about = {}
 if not VERSION:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
@@ -54,14 +51,13 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-
 )
