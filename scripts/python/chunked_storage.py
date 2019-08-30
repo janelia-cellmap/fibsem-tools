4e0b3bbac45136011ccacc60de1048d863472181
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-08-30 09:27:54 -0400

    partial transition to alternative implementation

--- scripts/python/chunked_storage.py
+++ scripts/python/chunked_storage.py
@@ -16,10 +16,27 @@ from dask.utils import SerializableLock
 from distributed import Client
 import time
 
-OUTPUT_FMTS = {"zarr", "n5"}
+OUTPUT_FMTS = {"n5"}
 max_chunksize = 1024
-
+compressor = numcodecs.GZip(level=1)
+lock = False
 # todo: move data writing functions to fst.io
+
+
+def save_blockwise(v, store_path, block_info):
+    # assumes input is 5D with first dimension == 1 and 
+    # second dimension == 2, and third + fourth dimensions the full size
+    # of the zarr/n5 file located at store_path
+    split_dim = 1
+    sinks = [zarr.open(store_path,mode='w')[f'/volumes/raw/ch{d}'] for d in range(2)]
+    
+    pos = block_info[0]['array-location']
+    # get rid of the channel dimension
+    pos.pop(split_dim)
+    idx = tuple(slice(*i) for i in pos)
+    for ind in range(2):
+        sinks[ind][idx] = np.expand_dims(v[0,ind], 0)
+    return np.zeros((1,) * v.ndim)
 
 
 def n5_store(
@@ -30,7 +47,6 @@ def n5_store(
     name: Union[str, list] = "raw",
 ) -> da.Array:
     store = zarr.N5Store(dest)
-    compressor = numcodecs.GZip(level=9)
     group = zarr.group(overwrite=True, store=store, path=path)
 
     # check that array is a list iff name is a list
@@ -61,29 +77,7 @@ def n5_store(
         result = array.store(z, compute=False, lock=lock)
     return result
 
-
-def zarr_store(
-    array: Union[da.Array, list],
-    metadata: list,
-    dest: str,
-    path: str = "/volumes",
-    name: Union[str, list] = "raw",
-) -> da.Array:
-    compressor = numcodecs.GZip(level=1)
-    group = zarr.group(overwrite=True, store=dest, path=path)
-
-    z = group.zeros(
-        shape=array.shape,
-        chunks=array.chunksize,
-        dtype=array.dtype,
-        compressor=compressor,
-        name=name,
-    )
-    z.attrs["metadata"] = metadata
-    return array.store(z, compute=False)
-
-
-stores = dict(n5=n5_store, zarr=zarr_store)
+stores = dict(n5=n5_store)
 
 
 def prepare_data(path: Union[str, list], max_chunksize: int) -> tuple:
@@ -104,19 +98,11 @@ def prepare_data(path: Union[str, list], max_chunksize: int) -> tuple:
         shapes.append(d.shape)
         dtypes.append(d.dtype)
     data = arrays_from_delayed(read(fnames, lazy=True), shapes=shapes, dtypes=dtypes)
-    stacked = padstack(data, constant_values="minimum-minus-one").swapaxes(0, 1)
+    #stacked = padstack(data, constant_values="minimum-minus-one").swapaxes(0, 1)
+    stacked = padstack(data, constant_values="minimum-minus-one")
+    rechunked = stacked
     # For raw data, single chunks along channel and z axes. Channels will become their own datasets
-    rechunked = stacked.rechunk(
-        (
-            1,
-            1,
-            *np.where(
-                np.array(stacked.shape[2:]) < max_chunksize,
-                stacked.shape[2:],
-                max_chunksize,
-            ),
-        )
-    )
+    #rechunked = stacked.rechunk((1,1,*np.where(np.array(stacked.shape[2:]) < max_chunksize, stacked.shape[2:], max_chunksize,),))
     logging.info(
         f"Assembled dataset with shape {rechunked.shape} using chunk sizes {rechunked.chunksize}"
     )
@@ -172,7 +158,7 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    lock = SerializableLock()
+    #lock = SerializableLock()
 
     logging.basicConfig(filename=args.log_file,
                         filemode='a',
@@ -187,8 +173,16 @@ if __name__ == "__main__":
 
     padded_array, metadata = prepare_data(args.source, max_chunksize=max_chunksize)
     dataset_path = '/volumes/raw'
-    dataset_names = ['ch' + str(r) for r in range(padded_array.shape[0])]
-    my_stores = stores[output_fmt]([*padded_array], metadata, args.dest, path=dataset_path, name=dataset_names)
+    dataset_names = ['ch' + str(r) for r in range(2)]
+    #my_stores = stores[output_fmt]([*padded_array], metadata, args.dest, path=dataset_path, name=dataset_names)
+    
+    # stick this in a function
+    group = zarr.group(overwrite=True, store=zarr.N5Store(args.dest), path=dataset_path)
+    # we are sending elements along the first axis to separate arrays 
+    out_shape = list(padded_array.shape)
+    out_shape.pop(1)
+    [group.zeros(name=n, shape=out_shape, compressor=compressor, chunks=(1,max_chunksize,max_chunksize)) for n in dataset_names]
+    import pdb; pdb.set_trace()
     if not args.dry_run:
         running_on_cluster = bsub_available()
         if running_on_cluster:
@@ -202,6 +196,7 @@ if __name__ == "__main__":
             client = Client(cluster)
 
         logging.info(f'Begin saving data to {args.dest}. View status at the following address: {cluster.dashboard_link}')
-        save_data(my_stores)
+        padded_array.map_blocks(save_blockwise, store_path=args.dest, dtype=padded_array.dtype).compute()
+        # save_data(my_stores)
         elapsed_time = time.time() - start_time
         logging.info(f'Save completed in {elapsed_time} s')
