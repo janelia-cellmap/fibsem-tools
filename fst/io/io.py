35c7161616fbe4cf0ffb5a39c653485de41a315a
Davis Vann Bennett <davis.v.bennett@gmail.com>
2019-09-10 19:30:58 -0400

    added reading arrays from containers using full paths instead of colon-separated paths

--- fst/io/io.py
+++ fst/io/io.py
@@ -6,47 +6,80 @@ from dask import delayed
 import os
 import h5py
 
+_container_extensions = ('.zarr', '.n5', '.h5')
+
+
+def split_path_at_container(path):
+    # check whether a path contains a valid file path to a container file, and if so which container format it is
+    result = None
+    pathobj = Path(path)
+    if pathobj.suffix in _container_extensions:
+        result = path
+    else:
+        for parent in pathobj.parents:
+            if parent.suffix in _container_extensions:
+                result = path.split(parent.suffix)
+                result[0] += parent.suffix
+    return result
+
 
 def split_path(path: str, sep: str = ':') -> List[str]:
     """
-    Split paths of the form `/foo/bar:baz` into `['foo/bar', 'baz'].
+    Split paths of the form `foo/bar.ext:baz` into `['foo/bar.ext', 'baz'] around the separator, e.g. `:`.
+    If there is no separator, split `foo/bar.ext/baz` into  `['foo/bar.ext', 'baz']` using `.ext` as a separator, where
+    `.ext` is a valid container extension
 
     Parameters
     ----------
     path: A string representing either a compound path (a/b/c:d) or a regular path (a/b/c)
-    sep: A string denoting the separator to use when splitting the input string.
+    sep: A string denoting the separator to use when splitting the input string. If the separator is not found in the
+    input path, a second attempt will be made to look for a supported container format extension and split the path at
+    that point.
 
     Returns a list of strings. If the separator is not found in the input string, the second string will be empty.
     -------
 
     """
     parts = path.split(sep)
+    result = [path, '']
     if len(parts) == 1:
-        parts.append('')
+        # look for a directory that ends one of the container formats
+        container_split = split_path_at_container(path)
+        if container_split is not None:
+            result = container_split
+        else:
+            parts.append('')
+            result = parts
+    elif len(parts) == 2:
+        result = parts
     elif len(parts) > 2:
         raise ValueError(f'Input string {path} contains too many instances of {sep}.')
-    return parts
+    return result
 
 
-def read_n5(dir_path: str, container_path: Union[str, None] = '') -> zarr.hierarchy.Group:
+def read_n5(dir_path: str, container_path: str = '') -> Union[zarr.hierarchy.Group, zarr.core.Array]:
     result = zarr.open(zarr.N5Store(dir_path), mode="r")
-    if container_path:
+    if container_path != '':
         result = result[container_path]
     return result
 
 
-def read_zarr(dir_path: str, container_path: Union[str, None] = '') -> zarr.hierarchy.Group:
+def read_zarr(dir_path: str, container_path: str = '') -> Union[zarr.hierarchy.Group, zarr.core.Array]:
     result = zarr.open(dir_path, mode="r")
-    if container_path:
+    if container_path != '':
         result = result[container_path]
     return result
 
 
-def read_h5(dir_path: str, container_path: Union[str, None] = '') ->  h5py._hl.files.File:
+def read_h5(dir_path: str, container_path: str = '') ->  Union[h5py._hl.files.File, h5py._hl.group.Group, h5py._hl.dataset.Dataset]:
     result = h5py.File(dir_path, mode="r")
-    if container_path:
+    if container_path != '':
         result = result[container_path]
     return result
+
+
+def access_n5(dir_path: str, container_path: str = '', **kwargs):
+    return zarr.open(zarr.N5Store(dir_path), path=container_path, **kwargs)
 
 
 readers = dict()
@@ -54,6 +87,28 @@ readers[".dat"] = read_fibsem
 readers[".n5"] = read_n5
 readers[".zarr"] = read_zarr
 readers[".h5"] = read_h5
+
+
+def access(path: Union[str, Iterable[str]], mode='r', lazy=False, **kwargs):
+    """
+    Enable reading and writing from array formats.
+
+    Parameters
+    ----------
+    path
+    mode
+    lazy
+
+    Returns an array-like object, a collection of array-like objects, a chunked store, or a dask.delayed object.
+    -------
+
+    """
+    if mode == 'r':
+        return read(path, lazy=lazy)
+    elif mode == 'w':
+        pass
+    elif mode == 'a':
+        pass
 
 
 def read(path: Union[str, Iterable[str]], lazy=False):
@@ -68,7 +123,7 @@ def read(path: Union[str, Iterable[str]], lazy=False):
     lazy: A boolean, defaults to False. If True, this function returns the native file reader wrapped by
     dask.delayed. This is advantageous for distributed computing.
 
-    Returns a single image, represented as an array-like object, a collection of array-like objects, a chunked store, or
+    Returns an array-like object, a collection of array-like objects, a chunked store, or
     a dask.delayed object.
     -------
 
