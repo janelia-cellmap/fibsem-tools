f53d02b7c97cb75c4aebb6f6d655f993b9958013
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-07-20 21:43:41 -0400

    black and fixed an outdated class reference"

--- fst/io/tensorstore.py
+++ fst/io/tensorstore.py
@@ -2,88 +2,98 @@ from dataclasses import dataclass, asdict
 from typing import Optional
 import tensorstore as ts
 
+
 @dataclass
-class KVStore():
+class KVStore:
     driver: str
     path: str
 
+
 @dataclass
-class ImageScaleMetadata():
+class ImageScaleMetadata:
     size: list
     chunk_size: list
     voxel_offset: Optional[list]
     resolution: list
     encoding: str
-    sharding: Optional[dict]  
-        
-@dataclass
-class MultiscaleMetadata():
-    data_type: str
-    num_channels: int
-    type: str       
+    sharding: Optional[dict]
+
 
 @dataclass
-class TensorStoreSpec():
+class MultiscaleMetadata:
+    data_type: str
+    num_channels: int
+    type: str
+
+
+@dataclass
+class TensorStoreSpec:
     driver: str
     kvstore: KVStore
     path: str
-    scale_metadata: ScaleMetadata
+    scale_metadata: ImageScaleMetadata
     multiscale_metadata: MultiscaleMetadata
-    
+
+
 import tensorstore as ts
 
-class TensorStoreArray():
-        def __init__(self,
-                 driver, 
-                 path, 
-                 kvstore_driver, 
-                 kvstore_path, 
-                 encoding, 
-                 num_channels, 
-                 volume_type,
-                 offset,
-                 resolution,
-                 *,
-                 dtype=None,
-                 size=None,
-                 template=None,                                
-                 chunk_size=None,                                  
-                 sharding=None):
 
-            if template is not None:
-                if size is not None and not np.array_equal(size, template.shape):
-                    raise ValueError('Must either specify size or data, not both')
+class TensorStoreArray:
+    def __init__(
+        self,
+        driver,
+        path,
+        kvstore_driver,
+        kvstore_path,
+        encoding,
+        num_channels,
+        volume_type,
+        offset,
+        resolution,
+        *,
+        dtype=None,
+        size=None,
+        template=None,
+        chunk_size=None,
+        sharding=None
+    ):
+    '''
+    A (relatively) friendly interface to tensorstore arrays. 
+    '''
 
-                size = template.shape
-                dtype = template.dtype.name
-                if chunk_size is None: 
-                    if hasattr(template, 'chunksize'):
-                        chunk_size = template.chunksize
-                    else:
-                        raise ValueError('Required argument `chunk_size` not supplied')
+        if template is not None:
+            if size is not None and not np.array_equal(size, template.shape):
+                raise ValueError("Must either specify size or data, not both")
 
+            size = template.shape
+            dtype = template.dtype.name
             if chunk_size is None:
-                raise ValueError('Required argument `chunk_size` not supplied.')
+                if hasattr(template, "chunksize"):
+                    chunk_size = template.chunksize
+                else:
+                    raise ValueError("Required argument `chunk_size` not supplied")
 
-            scale_metadata = ImageScaleMetadata(chunk_size=chunk_size, 
-                                           size=size,
-                                           voxel_offset=offset, 
-                                           resolution=resolution,
-                                           encoding=encoding,                                       
-                                           sharding=sharding)
+        if chunk_size is None:
+            raise ValueError("Required argument `chunk_size` not supplied.")
 
-            kvstore = KVStore(driver=kvstore_driver, 
-                              path=kvstore_path)
+        scale_metadata = ImageScaleMetadata(
+            chunk_size=chunk_size,
+            size=size,
+            voxel_offset=offset,
+            resolution=resolution,
+            encoding=encoding,
+            sharding=sharding,
+        )
 
-            multiscale_metadata = MultiscaleMetadata(data_type=dtype,
-                                                     num_channels=num_channels,
-                                                     type=volume_type)
+        kvstore = KVStore(driver=kvstore_driver, path=kvstore_path)
 
-            self.spec = asdict(TensorStoreSpec(driver, 
-                                   kvstore, 
-                                   path, 
-                                   scale_metadata, 
-                                   multiscale_metadata))
-            
-        def open(self, **kwargs):
-            return ts.open(self.spec, **kwargs)
+        multiscale_metadata = MultiscaleMetadata(
+            data_type=dtype, num_channels=num_channels, type=volume_type
+        )
+
+        self.spec = asdict(
+            TensorStoreSpec(driver, kvstore, path, scale_metadata, multiscale_metadata)
+        )
+
+    def open(self, **kwargs):
+        return ts.open(self.spec, **kwargs)
