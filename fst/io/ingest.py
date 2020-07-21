a52c580951749fa2267d98533f76ef2676484bf8
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-07-20 21:37:36 -0400

    black

--- fst/io/ingest.py
+++ fst/io/ingest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import dask.array as da
 
+
 def pad_arrays(arrays, constant_values, stack=True):
     """
     Pad arrays with variable axis sizes. A bounding box is calculated across all the arrays and each sub-array is
@@ -30,18 +31,29 @@ def pad_arrays(arrays, constant_values, stack=True):
 
     # pad elements of the first axis differently
     def padfun(array, pad_width, constant_values):
-        return np.stack([np.pad(a, pad_width, constant_values=cv) for a, cv in zip(array, constant_values)])
+        return np.stack(
+            [
+                np.pad(a, pad_width, constant_values=cv)
+                for a, cv in zip(array, constant_values)
+            ]
+        )
+
     # If all the shapes are identical no padding is needed.
     if np.unique(shapes, axis=0).shape[0] == 1:
         padded = arrays
     else:
         padded = [
-                a.map_blocks(padfun,
-                             pad_width=pad_extent[ind][1:],
-                             constant_values=constant_values,
-                             chunks=tuple(c + p[1] - p[0] for c, p in zip(a.chunksize, pad_extent[ind])),
-                             dtype=a.dtype)
-                for ind, a in enumerate(arrays)]
+            a.map_blocks(
+                padfun,
+                pad_width=pad_extent[ind][1:],
+                constant_values=constant_values,
+                chunks=tuple(
+                    c + p[1] - p[0] for c, p in zip(a.chunksize, pad_extent[ind])
+                ),
+                dtype=a.dtype,
+            )
+            for ind, a in enumerate(arrays)
+        ]
 
     return padded, pad_extent
 
@@ -78,5 +90,3 @@ def arrays_from_delayed(args, shapes=None, dtypes=None):
         for ind in range(len(args))
     ]
     return arrays
-
-
