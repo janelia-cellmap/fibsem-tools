eab8c32a7c1e8356f5c0dd19e07e1a449afebd30
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-07-20 21:40:01 -0400

    added fwalk to top-level io module

--- fst/io/__init__.py
+++ fst/io/__init__.py
@@ -1,2 +1,2 @@
-from .io import read, access, rmtree_parallel, create_arrays
+from .io import read, access, rmtree_parallel, create_arrays, fwalk
 from .ingest import pad_arrays, arrays_from_delayed
