aaa839b1bb2eebe03a2cf24b8ebc07ce37168e96
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-06-18 15:04:03 -0400

    add delitems and restore a previously slow path in delitem (which we might remove again if it's too slow)

--- src/fibsem_tools/io/storage.py
+++ src/fibsem_tools/io/storage.py
@@ -144,10 +144,16 @@ class FSStore(MutableMapping):
             raise ReadOnlyError()
         key = self._normalize_key(key)
         path = self.dir_path(key)
-        #if self.fs.isdir(path):
-        #    self.fs.rm(path, recursive=True)
-        #else:
-        del self.map[key]
+        if self.fs.isdir(path):
+            self.fs.rm(path, recursive=True)
+        else:
+            del self.map[key]
+
+    def delitems(self, keys):
+        if self.mode == 'r':
+            raise ReadOnlyError()
+        nkeys = [self._normalize_key(key) for key in keys]
+        self.map.delitems(nkeys)
 
     def __contains__(self, key):
         key = self._normalize_key(key)
