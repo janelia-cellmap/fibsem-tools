9561a29cb70487e6dab1d92d23d42e7dfc47b696
Davis Vann Bennett <davis.v.bennett@gmail.com>
2021-07-07 15:51:21 -0400

    fix name param applying to wrong scope

--- src/fibsem_tools/metadata/cosem.py
+++ src/fibsem_tools/metadata/cosem.py
@@ -3,12 +3,14 @@ from typing import Sequence, List, Optional
 from xarray import DataArray
 from .transform import SpatialTransform
 
+
 class ScaleMeta(BaseModel):
     path: str
     transform: SpatialTransform
 
 
 class MultiscaleMeta(BaseModel):
+    name: Optional[str]
     datasets: Sequence[ScaleMeta]
 
 
@@ -17,11 +19,15 @@ class COSEMGroupMetadata(BaseModel):
     Multiscale metadata used by COSEM for multiscale datasets saved in N5/Zarr groups.
     """
 
-    name: str
     multiscales: Sequence[MultiscaleMeta]
 
     @classmethod
-    def fromDataArrays(cls, dataarrays: Sequence[DataArray], name: str, paths: Optional[Sequence[str]]):
+    def fromDataArrays(
+        cls,
+        dataarrays: Sequence[DataArray],
+        name: Optional[str] = None,
+        paths: Optional[Sequence[str]] = None,
+    ):
         """
         Generate multiscale metadata from a list or tuple of DataArrays.
 
@@ -31,9 +37,9 @@ class COSEMGroupMetadata(BaseModel):
         dataarrays : list or tuple of xarray.DataArray
             The collection of arrays from which to generate multiscale metadata. These arrays are assumed to share the same `dims` attributes, albeit with varying `coords`.
 
-        name : str
+        name : str, optional
             The name for the multiresolution collection
-        
+
         paths : list or tuple of str or None, default=None
             The name on the storage backend for each of the arrays in the multiscale collection. If None, the `name` attribute of each array in `dataarrays` will be used.
 
@@ -42,7 +48,7 @@ class COSEMGroupMetadata(BaseModel):
 
         COSEMGroupMetadata
         """
-        
+
         if paths is None:
             _paths: Sequence[str] = [str(d.name) for d in dataarrays]
         else:
@@ -50,10 +56,11 @@ class COSEMGroupMetadata(BaseModel):
 
         multiscales = [
             MultiscaleMeta(
+                name=name,
                 datasets=[
                     ScaleMeta(path=path, transform=SpatialTransform.fromDataArray(arr))
                     for path, arr in zip(paths, dataarrays)
-                ]
+                ],
             )
         ]
         return cls(name=name, multiscales=multiscales, paths=_paths)
