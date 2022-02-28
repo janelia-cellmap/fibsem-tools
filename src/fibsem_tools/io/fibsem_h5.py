1465176d8446c8af22cf57393d6025e77cdd7cf7
kittisopikulm <kittisopikulm@janelia.hhmi.org>
2022-02-28 15:19:37 -0500

    Fix _extract_raw_header reference, use numpy style docs

--- src/fibsem_tools/io/fibsem_h5.py
+++ src/fibsem_tools/io/fibsem_h5.py
@@ -21,6 +21,7 @@ from .fibsem import (
     OFFSET,
     MAGIC_NUMBER,
     read_fibsem,
+    _extract_raw_header,
 )
 
 chunk_type = Union[Tuple[int, int], Tuple[int, int, int]]
@@ -32,16 +33,28 @@ def create_fibsem_h5_dataset(
     data: FIBSEMData,
     chunks: chunk_type = None,
     **kwargs
-):
+) -> h5py.Dataset:
     """
     Create a HDF5 dataset from an existing FIBSEMData object.
 
-    :param parent: `h5py.File` or `h5py.Group` to place the dataset in
-    :param name: Name of the dataset
-    :param data: `FIBSEMData` instance
-    :param chunks: tuple describing the chunk size (Y, X)
-    :type chunks: tuple, optional
-    :param **kwargs: Keyword arguments are forwarded to `parent.create_dataset`
+    Parameters
+    ----------
+    parent
+        `h5py.File` or `h5py.Group` to place the dataset in
+    name
+        Name of the dataset
+    data
+        `FIBSEMData` instance
+    chunks
+        tuple describing the chunk size (Y, X) or (C, Y, X)
+    **kwargs
+        Keyword arguments are forwarded to `parent.create_dataset`
+
+    Returns
+    -------
+    ds
+        h5py.Dataset encoding the attributes and data
+
     """
 
     # If chunks has length 2, add a 1 for the ChanNum dimension
@@ -64,15 +77,28 @@ def create_fibsem_h5_dataset_external(
     data: FIBSEMData,
     datfile: str,
     **kwargs
-):
+) -> h5py.Dataset:
     """
     Create a HDF5 external dataset where the data is still stored in the dat file.
     Note that the axes is rolled in the dat file versus FIBSEMData object
 
-    :param parent: `h5py.File` or `h5py.Group` to place the dataset in
-    :param name: Name of the dataset
-    :param data: `FIBSEMData` instance
-    :param **kwargs: Keyword arguments are forwarded to `parent.create_dataset`
+    Parameters
+    ----------
+    parent
+        `h5py.File` or `h5py.Group` to place the dataset in
+    name
+        Name of the dataset
+    data
+        `FIBSEMData` instance
+    datfile
+        File name of the dat file
+    **kwargs
+        Keyword arguments are forwarded to `parent.create_dataset`
+
+    Returns
+    -------
+    ds
+        h5py.Dataset with data stored in the original dat file
     """
 
     # FIBSEMData is usually (ChanNum, YResolution, XResolution)
@@ -84,7 +110,7 @@ def create_fibsem_h5_dataset_external(
     parent.create_dataset(
         name,
         data.shape,
-        ">i2",
+        data.dtype,
         external=[(datfile, 1024, h5py.h5f.UNLIMITED)],
         **kwargs
     )
@@ -96,16 +122,27 @@ def create_fibsem_h5_dataset_external(
 
 def create_fibsem_h5_file(
     h5_filename: str, dataset_name: str, data: FIBSEMData, force: bool = False, **kwargs
-):
+) -> None:
     """
     Create HDF5 file with a single dataset
 
-    :param h5_filename: Name of the HDF5 file to create
-    :param dataset_name: Name of the dataset
-    :param data: `FIBSEMData` instance
-    :param force: Overrwrite existing HDF5 file if True. defaults to False.
-    :type force: bool, optional
-    :param **kwargs: Keyword arguments are forwarded to `parent.create_dataset`
+    Parameters
+    ----------
+    h5_filename
+        Name of the HDF5 file to create
+    dataset_name
+        Name of the dataset
+    data
+        `FIBSEMData` instance
+    overwrite
+        Overwrite existing HDF5 file if True. defaults to False.
+    **kwargs
+        Keyword arguments are forwarded to `parent.create_dataset`
+
+    Returns
+    -------
+    None
+
     """
     # Use 'x' file permissions so we do not overwrite files
     mode = "w" if force else "x"
@@ -114,23 +151,33 @@ def create_fibsem_h5_file(
     f.close()
 
 
-def add_raw_header_attr(dataset: h5py.Dataset, filename: str):
+def add_raw_header_attr(dataset: h5py.Dataset, filename: str) -> None:
     """
     Extract header from filename and add it as an attribute to a HDF5 dataset
 
-    :param dataset: `h5py.Dataset` to which to add an attribute "RawHeader"
+    Parameters
+    ----------
+    dataset
+        `h5py.Dataset` to which to add an attribute "RawHeader"
     """
     rawheader = _extract_raw_header(filename)
     dataset.attrs["RawHeader"] = np.frombuffer(rawheader, dtype="u1")
 
 
-def load_fibsem_from_h5_dataset(dataset: h5py.Dataset):
+def load_fibsem_from_h5_dataset(dataset: h5py.Dataset) -> FIBSEMData:
     """
     Create a FIBSEMData instance from a HDF5 dataset
 
-    :param dataset: `h5py.Dataset` to load to create a FIBSEMData instance.
+    Parameters
+    ----------
+    dataset
+        `h5py.Dataset` to load to create a FIBSEMData instance.
 
-    :returns: A `FIBSEMData` instance. Metadata are loaded from HDF5 attributes, not from the raw header.
+    Returns
+    -------
+    data
+        A `FIBSEMData` instance. Metadata are loaded from HDF5 attributes, not from the raw header.
+
     """
     header = FIBSEMHeader(**dataset.attrs)
     if dataset.shape == (header.ChanNum, header.YResolution, header.XResolution):
@@ -149,19 +196,28 @@ def create_aggregate_fibsem_h5_file(
     raw_headers: Sequence[bytes] = None,
     force: bool = False,
     **kwargs
-):
+) -> str:
     """
     Create aggregate FIBSEM HDF5 File
 
-    :param h5_filename: Filename of the HDF5 file to create.
-    :param dataset_names: Names of the datasets to create
-    :param data: A sequence of FIBSEMData instances.
-    :param raw_headers: Raw headers in the dat file.
-    :type raw_headers: bytes, optional
-    :param force: Overrwrite existing HDF5 file if True. defaults to False.
-    :type force: bool, optional
+    Parameters
+    ----------
+    h5_filename
+        Filename of the HDF5 file to create.
+    dataset_names
+        Names of the datasets to create
+    data
+        A sequence of FIBSEMData instances.
+    raw_headers
+        Raw headers in the dat file.
+    overwrite
+        Overwrite existing HDF5 file if True. defaults to False.
 
-    :returns: h5_filename
+    Returns
+    -------
+    h5_filename
+        Filename of the created HDF5 file
+
     """
     # Use 'x' permissions so we do not overwrite files
     mode = "w" if force else "x"
@@ -179,15 +235,21 @@ def create_aggregate_fibsem_h5_file(
 
 def create_aggregate_fibsem_h5_file_from_dat_filenames(
     dat_filenames: Sequence[str], h5_dirname: str = None, **kwargs
-):
+) -> str:
     """
     Create aggregate FIBSEM HDF5 File from .dat filenames
 
-    :param dat_filenames: A sequence of dat filenames
-    :param h5_filename: Name of HDF5 FILE. defaults to same directory as dat_filenames[0]
-    :type h5_filename: str, optional
+    Parameters
+    ----------
+    dat_filenames
+        A sequence of dat filenames
+    h5_filename
+        Name of HDF file. defaults to same directory as dat_filenames[0]
 
-    :returns: h5_filename
+    Returns
+    -------
+    h5_filename
+        File name of the HDF5 file
     """
     prefix_name = ""
     dataset_names = []
@@ -216,16 +278,24 @@ def create_aggregate_fibsem_h5_file_from_dat_filenames(
     )
 
 
-def create_default_h5_files(source_data, **kwargs):
+def create_default_h5_files(source_data, **kwargs) -> Sequence[str]:
     """
     Create HDF5 files with default settings
 
     This is a high-level convenience function.
 
-    :param source_data: Source data. .dat filenames, a directory name, or a glob
-    :param chunk: Chunk size as tuple (Y, X)
+    Parameters
+    ----------
+    source_data
+        Source data. .dat filenames, a directory name, or a glob
+    chunk
+        Chunk size as tuple (Y, X)
 
-    :return: list of HDF5 filenames or `None`
+    Returns
+    -------
+    h5_files
+       list of HDF5 filenames or `None`
+
     """
     if os.path.isdir(source_data):
         source_data = os.path.join(source_data, "*.dat")
@@ -261,17 +331,23 @@ def create_default_h5_files(source_data, **kwargs):
     return None
 
 
-def create_zstd_h5_files(source_data, aggression=1, **kwargs):
+def create_zstd_h5_files(source_data, aggression=1, **kwargs) -> Sequence[str]:
     """
     Create HDF5 files with ZSTD compression.
 
     This is a high-level convenience function.
 
-    :param source_data: Source data. .dat filenames, directory, or glob
-    :param aggression: Zstd aggression level. defaults to 1
-    :type aggression: int, optional
+    Parameters
+    ----------
+    source_data
+        Source data. .dat filenames, directory, or glob
+    aggression
+        Zstd aggression level. defaults to 1
 
-    :return: list of HDF5 filenames or `None`
+    Returns
+    -------
+    h5_files
+        list of HDF5 filenames or `None`
     """
 
     return create_default_h5_files(
@@ -282,17 +358,23 @@ def create_zstd_h5_files(source_data, aggression=1, **kwargs):
     )
 
 
-def create_gzip_h5_files(source_data, aggression=1, **kwargs):
+def create_gzip_h5_files(source_data, aggression=1, **kwargs) -> Sequence[str]:
     """
     Create HDF5 files with built-in GZIP compression
 
     This is a high-level convenience function.
 
-    :param source_data: Source data. .dat filenames, directory, or glob
-    :param aggression: Gzip aggression level. defaults to 1
-    :type aggression: int, optional
+    Parameters
+    ----------
+    source_data
+        Source data. .dat filenames, directory, or glob
+    aggression
+        aggression: Gzip aggression level. defaults to 1
 
-    :return: list of HDF5 filenames or `None`
+    Returns
+    -------
+    h5_files
+        list of HDF5 filenames or `None`
     """
     return create_default_h5_files(
         source_data, compression="gzip", compression_opts=aggression, **kwargs
