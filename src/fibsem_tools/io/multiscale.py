586e16f62fe252b8ac264d68e90b6ba0869d6ec5
Davis Vann Bennett <davis.v.bennett@gmail.com>
2023-02-06 10:45:38 -0500

    add functions for multiscale storage creation

--- src/fibsem_tools/io/multiscale.py
+++ src/fibsem_tools/io/multiscale.py
@@ -1,18 +1,29 @@
 import os
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, Literal, Optional, Sequence, Tuple, Union, List
+from pathlib import Path
+from urllib.parse import urlparse
 
 import distributed
 import numpy as np
 from numpy.typing import NDArray
 from xarray import DataArray
 
-from fibsem_tools.io import initialize_group
+from fibsem_tools.io import initialize_group, access
 from fibsem_tools.io.dask import store_blocks, write_blocks
 from fibsem_tools.io.io import AccessMode
 from fibsem_tools.io.zarr import lock_array
 from fibsem_tools.metadata.cosem import COSEMGroupMetadata
 from fibsem_tools.metadata.neuroglancer import NeuroglancerN5GroupMetadata
 from fibsem_tools.metadata.transform import SpatialTransform
+from zarr.errors import ContainsGroupError
+from numcodecs.abc import Codec
+
+Attrs = Dict[str, Any]
+JSON = Union[dict[str, "JSON"], list["JSON"], str, int, float, bool, None]
+
+PathLike = Union[str, Path]
+
+multiscale_metadata_types = ["neuroglancer", "cellmap", "cosem"]
 
 
 class Multiscales:
@@ -197,3 +208,171 @@ def _normalize_chunks(
     assert len(result) == len(arrays)
     assert tuple(map(len, result)) == tuple(x.ndim for x in arrays)
     return result
+
+
+def create_multiscale_metadata(
+    arrays: Sequence[DataArray], metadata_types: List[str]
+) -> Tuple[JSON, JSON]:
+    """
+    Given a list of xarray DataArray, generate multiscale metadata of the desired flavor.
+
+    Returns
+    -------
+
+    A tuple of dicts with string keys and JSON-serializable values
+
+    """
+    group_attrs = {}
+    array_attrs = [{}] * len(arrays)
+    for flavor in set(metadata_types):
+        if flavor == "neuroglancer":
+            g_meta = NeuroglancerN5GroupMetadata.fromDataArrays(arrays)
+            group_attrs.update(g_meta.dict())
+        elif flavor == "cellmap" or flavor == "cosem":
+            g_meta = COSEMGroupMetadata.fromDataArrays(arrays)
+            group_attrs.update(g_meta.dict())
+            for idx in range(len(array_attrs)):
+                array_attrs[idx] = {
+                    **SpatialTransform.fromDataArray(arrays[idx]).dict(),
+                    **array_attrs[idx],
+                }
+        else:
+            raise ValueError(
+                f"Multiscale metadata type {flavor} is unknown. Try one of {multiscale_metadata_types}"
+            )
+    return group_attrs, array_attrs
+
+
+def create_group(
+    group_url: PathLike,
+    arrays: Sequence[NDArray[Any]],
+    array_paths: Sequence[str],
+    chunks: Sequence[int],
+    group_attrs: Attrs = {},
+    array_attrs: Optional[Sequence[Attrs]] = None,
+    group_mode: AccessMode = "w-",
+    array_mode: AccessMode = "w-",
+    **array_kwargs,
+) -> Tuple[str, Tuple[str, ...]]:
+
+    bad_paths = []
+    for path in array_paths:
+        if len(Path(path).parts) > 1:
+            bad_paths.append(path)
+
+    if len(bad_paths):
+        raise ValueError(
+            f"Array paths cannot be nested. The following paths violate this rule: {bad_paths}"
+        )
+    protocol = urlparse(group_url).scheme
+    protocol_prefix = ""
+    if protocol != "":
+        protocol_prefix = protocol + "://"
+    group = access(group_url, mode=group_mode, attrs=group_attrs)
+
+    if array_attrs is None:
+        _array_attrs: Tuple[Attrs, ...] = ({},) * len(arrays)
+    else:
+        _array_attrs = array_attrs
+
+    for idx, array in enumerate(arrays):
+        name = array_paths[idx]
+        path = protocol_prefix + os.path.join(group.store.path, group.path, name)
+        z_arr = access(
+            path=path,
+            mode=array_mode,
+            shape=array.shape,
+            dtype=array.dtype,
+            chunks=chunks[idx],
+            attrs=_array_attrs[idx],
+            **array_kwargs,
+        )
+    g_url = protocol_prefix + os.path.join(group.store.path, group.path)
+    a_urls = [os.path.join(g_url, name) for name in array_paths]
+
+    return g_url, a_urls
+
+
+def create_multiscale_group(
+    group_url: str,
+    arrays: List[DataArray],
+    array_paths: List[str],
+    chunks: Optional[Union[int, Tuple[Tuple[int, ...]]]],
+    metadata_types: List[str],
+    group_mode="w-",
+    array_mode="w-",
+    group_attrs: Optional[Attrs] = None,
+    array_attrs: Optional[Sequence[Attrs]] = None,
+    **kwargs,
+) -> Tuple[str, Tuple[str, ...]]:
+    if array_attrs is None:
+        array_attrs = [{}] * len(arrays)
+    if group_attrs is None:
+        group_attrs = {}
+
+    mgroup_attrs, marray_attrs = create_multiscale_metadata(arrays, metadata_types)
+    group_attrs.update(mgroup_attrs)
+    [a.update(marray_attrs[idx]) for idx, a in enumerate(array_attrs)]
+
+    _chunks = _normalize_chunks(arrays, chunks)
+    try:
+        paths = create_group(
+            group_url,
+            arrays,
+            array_paths=array_paths,
+            chunks=_chunks,
+            group_attrs=group_attrs,
+            array_attrs=array_attrs,
+            group_mode=group_mode,
+            array_mode=array_mode,
+            **kwargs,
+        )
+        return paths
+    except ContainsGroupError:
+        raise FileExistsError(
+            f"""
+            The resource at {group_url} resolves to an existing group. 
+            Use 'w' or 'a' access modes to enable writable / appendable access to this group.
+            """
+        )
+
+
+def prepare_multiscale(
+    scratch_url: str,
+    dest_url: str,
+    arrays: List[DataArray],
+    array_names: List[str],
+    access_mode: Literal["w", "w-", "a"],
+    metadata_types: List[str],
+    store_chunks: Tuple[int, ...],
+    compressor: Codec,
+) -> Tuple[str, str]:
+
+    # prepare the temporary storage
+    scratch_names = array_names[1:]
+    scratch_multi = arrays[1:]
+
+    scratch_group_url, scratch_array_urls = create_multiscale_group(
+        scratch_url,
+        scratch_multi,
+        scratch_names,
+        chunks=None,
+        metadata_types=metadata_types,
+        group_mode="w",
+        array_mode="w",
+        compressor=None,
+    )
+
+    # prepare final storage
+    dest_group_url, dest_array_urls = create_multiscale_group(
+        dest_url,
+        arrays,
+        array_names,
+        chunks=store_chunks,
+        metadata_types=metadata_types,
+        group_mode=access_mode,
+        array_mode=access_mode,
+        compressor=compressor,
+    )
+
+    return scratch_array_urls, dest_array_urls
