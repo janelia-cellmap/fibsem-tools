9cf1fdf99e49e041839dd9010a0a90fca8343f53
Davis Vann Bennett <davis.v.bennett@gmail.com>
2023-03-23 14:14:44 -0400

    single-source version string

--- src/fibsem_tools/__init__.py
+++ src/fibsem_tools/__init__.py
@@ -1,4 +1,6 @@
+from importlib.metadata import version as _version
+
+__version__ = _version(__name__)
+
 # ruff: noqa
 from fibsem_tools.io.core import read, read_dask, read_xarray, access, create_group
-
-__version__ = "0.2.8"
