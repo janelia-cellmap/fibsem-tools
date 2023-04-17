986834387246d5ae92251e3905b0e50db6009c89
Talley Lambert <talley.lambert@gmail.com>
2023-04-17 13:38:25 -0400

    add dep

--- src/fibsem_tools/__init__.py
+++ src/fibsem_tools/__init__.py
@@ -4,3 +4,13 @@ __version__ = _version(__name__)
 
 # ruff: noqa
 from fibsem_tools.io.core import read, read_dask, read_xarray, access, create_group
+from fibsem_tools._dataset import CosemDataset
+
+__all__ = [
+    "access",
+    "create_group",
+    "read",
+    "read_dask",
+    "read_xarray",
+    "CosemDataset",
+]
