bb07ffc90fb3c807b5d81aefda25d6616e3329ff
Davis Vann Bennett <davis.v.bennett@gmail.com>
2023-08-24 17:15:12 -0400

    feat: global cli access

--- src/fibsem_tools/cli/zarr2json.py
+++ src/fibsem_tools/cli/zarr2json.py
@@ -6,23 +6,25 @@ from fibsem_tools import read
 from fibsem_tools.io.util import split_by_suffix
 from rich import print
 
+
 def parse_zarr_path(path: str) -> Union[ArraySpec, GroupSpec]:
-    pre, post, suffix = split_by_suffix(path, ('.n5', '.zarr'))
+    pre, post, suffix = split_by_suffix(path, (".n5", ".zarr"))
     obj = read(path)
     if isinstance(obj, zarr.Array):
         result = ArraySpec.from_zarr(obj)
     elif isinstance(obj, zarr.Group):
         result = GroupSpec.from_zarr(obj)
     else:
-        raise ValueError(f'Got an unparseable object: {type(obj)}')
+        raise ValueError(f"Got an unparseable object: {type(obj)}")
     return result
 
 
 @click.command()
-@click.argument('path', type=click.STRING)
+@click.argument("path", type=click.STRING)
 def cli(path: str):
     result = parse_zarr_path(path)
     print(result.json(indent=2))
+
 
 if __name__ == "__main__":
     cli()
