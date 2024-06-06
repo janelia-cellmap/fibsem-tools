d135704db13eb6adced00e41da5eb9dda85d08d9
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-06-06 04:54:10 -0400

    clean out poorly implemented array copy routines. we can add these again if someone complains

--- src/fibsem_tools/cli/fst.py
+++ src/fibsem_tools/cli/fst.py
@@ -1,14 +1,6 @@
 import click
 
-from fibsem_tools.cli.array_copy import copy_array_cli, copy_group_cli
-from fibsem_tools.cli.save_single_chunked import save_single_chunked_cli
-
 
 @click.group("fst")
 def fst():
     pass
-
-
-fst.add_command(copy_array_cli)
-fst.add_command(copy_group_cli)
-fst.add_command(save_single_chunked_cli, "save-single-chunked")
